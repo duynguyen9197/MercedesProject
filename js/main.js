da4fd163394f3bc1f856d1c93c232c979a0bfebb
LeHuynhQui <lehuynhqui68@gmail.com>
2021-05-26 09:26:23 +0700

    complete footer and added js scroll top, changed boostrap 4.0 to 4.6

--- js/main.js
+++ js/main.js
@@ -1,34 +1,42 @@
 // Xử lý show more của New
-const showMoreNew = document.querySelector('.showMoreNew')
-const hiddenPictureNew = document.querySelectorAll('.hidden-picture-new')
+const showMoreNew = document.querySelector('.showMoreNew');
+const hiddenPictureNew = document.querySelectorAll('.hidden-picture-new');
 
 showMoreNew.addEventListener('click', e =>{
-  showMoreNew.classList.add('d-none')
+  showMoreNew.classList.add('d-none');
   for (var i = 0; i < hiddenPictureNew.length; ++i) {
-    hiddenPictureNew[i].classList.remove('d-none')
+    hiddenPictureNew[i].classList.remove('d-none');
   }
-})
+});
 
 
 // Xử lý show more của Popular
-const showMorePopular = document.querySelector('.showMore-popular')
-const hiddenPicturePopular = document.querySelectorAll('.hidden-picture-popular')
+const showMorePopular = document.querySelector('.showMore-popular');
+const hiddenPicturePopular = document.querySelectorAll('.hidden-picture-popular');
 
 showMorePopular.addEventListener('click', e =>{
-  showMorePopular.classList.add('d-none')
+  showMorePopular.classList.add('d-none');
   for (var i = 0; i < hiddenPicturePopular.length; ++i) {
-    hiddenPicturePopular[i].classList.remove('d-none')
+    hiddenPicturePopular[i].classList.remove('d-none');
   }
-})
+});
 
 
 // Xử lý show more của Videos
-const showMoreVideos = document.querySelector('.showMore-videos')
-const hiddenPictureVideos = document.querySelectorAll('.hidden-picture-videos')
+const showMoreVideos = document.querySelector('.showMore-videos');
+const hiddenPictureVideos = document.querySelectorAll('.hidden-picture-videos');
 
 showMoreVideos.addEventListener('click', e =>{
-  showMoreVideos.classList.add('d-none')
+  showMoreVideos.classList.add('d-none');
   for (var i = 0; i < hiddenPictureVideos.length; ++i) {
-    hiddenPictureVideos[i].classList.remove('d-none')
+    hiddenPictureVideos[i].classList.remove('d-none');
   }
+});
+
+
+//Xử lý ấn nút top thì sẽ scroll top
+const topBtn = document.querySelector('.top')
+topBtn.addEventListener('click', e =>{
+    window.scrollTo({top: 0})
 })
+
